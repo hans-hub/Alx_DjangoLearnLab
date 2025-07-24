50a839c6461aa62f2b3b4deaf9bd35e802e41fc1
Hans Kyei <baffourhans@gmail.com>
2025-07-24 03:42:52 +0000

    updated relationship_app urls.py to include a new path for listing books

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .views import list_books
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # FBV
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV
