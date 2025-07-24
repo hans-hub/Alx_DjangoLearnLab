b4565279428c4671466e4a8c5bf99b799883458f
Hans Kyei <baffourhans@gmail.com>
2025-07-24 13:58:13 +0000

    update relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
