8a315aabae64b2fbbe805d6d99260bd69a13fdbe
Hans Kyei <baffourhans@gmail.com>
2025-07-24 03:39:39 +0000

    edited the views and urls files to ensure the correct function names are used in the URL patterns. The changes include: - Updated the `urls.py` in the `relationship_app` to use `list_books` as the view function for the 'books/' path. - Added a root URL pattern in the main `urls.py` to point to `list_books`, ensuring that the home page displays the list of books. - Ensured that the `urls.py` in the main project includes the `relationship_app

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from relationship_app import views  # Import views from the bookshelf app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship/', include('relationship_app.urls')),
-]
+    
+    ]
 
 
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list, name='list_book'),  # FBV
+    path('books/', views.list_books, name='list_books'),  # FBV
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV
 ]
