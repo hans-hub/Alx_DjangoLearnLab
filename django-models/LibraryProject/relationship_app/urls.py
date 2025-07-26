03059969e8903dd57cdc9ed5f69f553532155a40
Hans Kyei <baffourhans@gmail.com>
2025-07-26 12:22:35 +0000

    changes to views.py and urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('admin-area/', views.admin_view, name='admin_view'),
-   #path('librarian-area/', views.librarian_view, name='librarian_view'),
-   #path('member-area/', views.member_view, name='member_view'),
+    path('librarian-area/', views.librarian_view, name='librarian_view'),
+    path('member-area/', views.member_view, name='member_view'),
 ]
