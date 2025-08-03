a5932798b7c026638b10c38f1af6b0e8dcf3cf0d
Hans Kyei <baffourhans@gmail.com>
2025-08-03 22:19:32 +0000

    added urls.puy and views.py to the api_project/api directory

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include   
 from api.views import BookList  # Import the BookList view
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api.views import BookViewSet  # Import the BookViewSet
 
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,viewsets
 from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Book    
 from .serializers import BookSerializer 
@@ -11,3 +12,7 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer   
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
