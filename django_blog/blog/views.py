ea804fd2589482cf57843ac9429b42a4a1095c73
Hans Kyei <baffourhans@gmail.com>
2025-08-17 21:13:45 +0000

    added # User authentication and profile management

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email')
+        request.user.save()
+    return render(request, 'blog/profile.html')
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,3 +122,7 @@ STATICFILES_DIRS = [BASE_DIR / "blog/static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
