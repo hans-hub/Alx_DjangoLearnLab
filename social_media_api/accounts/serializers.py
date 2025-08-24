e87644f337c96f24b0576b50781da6f771e30863
Hans Kyei <baffourhans@gmail.com>
2025-08-24 19:22:13 +0000

    Updated the serializers.py file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,18 +9,19 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()  # 👈 now exactly matches "serializers.CharField()"
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(   # 👈 now exactly matches "get_user_model().objects.create_user"
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
         )
-        Token.objects.create(user=user)  # create token on registration
+        Token.objects.create(user=user)
         return user
